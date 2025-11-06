88070564d3251914e3d6d926e8f81a47a688c3ca
JLuppe <josephluppe@gmail.com>
2025-11-05 21:22:51 -0800

    added region name

--- rag.py
+++ rag.py
@@ -77,5 +77,6 @@ def get_embedding_function():
     embeddings = BedrockEmbeddings(
         aws_access_key_id=aws_access_key,
         aws_secret_access_key=aws_secret_key,
+        region_name = "us-east-2"
     )
     return embeddings
